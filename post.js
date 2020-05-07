6249522d7c9b6e8b7db3028df4cda46255b52707
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-07 11:41:40 +0200

    add post script

---
+++ post.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const fs = require('fs')
+
+function rm(file) {
+    core.info(`==> Deleting "${file}" file`)
+    fs.unlinkSync(file)
+}
+
+async function main() {
+    try {
+        const keyFile = core.getState("keyFile")
+        const inventoryFile = core.getState("inventoryFile")
+        const vaultPasswordFile = core.getState("vaultPasswordFile")
+
+        if (keyFile)
+            rm(keyFile)
+
+        if (inventoryFile)
+            rm(inventoryFile)
+
+        if (vaultPasswordFile)
+            rm(vaultPasswordFile)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
