d8a8e9121d79891b428835b217cb9c0add035a36
Brooks Swinnerton <bswinnerton@gmail.com>
2021-07-30 21:50:09 -0400

    Consolidate stdout and stderr into single output

--- main.js
+++ main.js
@@ -81,21 +81,19 @@ async function main() {
 
         process.env.ANSIBLE_FORCE_COLOR = "True"
 
-        let stdout = ''
-        let stderr = ''
+        let output = ''
         const execOptions = {}
         execOptions.listeners = {
           stdout: function(data) {
-            stdout += data.toString()
+            output += data.toString()
           },
           stderr: function(data) {
-            stderr += data.toString()
+            output += data.toString()
           }
         }
 
         await exec.exec("ansible-playbook", cmd, execOptions)
-        core.setOutput('stdout', stdout)
-        core.setOutput('stderr', stderr)
+        core.setOutput('output', output)
     } catch (error) {
         core.setFailed(error.message)
     }
